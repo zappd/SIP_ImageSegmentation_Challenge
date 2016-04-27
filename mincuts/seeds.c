f35a967a9699b56f50b790c978a75a152a851627
Alex Nichols <anichols@cellassist.com>
2016-04-27 10:50:41 -0400

    Added seed struct definition

---
+++ mincuts/seeds.c
@@ -0,0 +1,4 @@
+//
+// Created by root on 4/27/16.
+//
+
---
+++ mincuts/seeds.h
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+//
+// Created by root on 4/27/16.
+//
+
+#ifndef MINCUTS_SEEDS_H
+#define MINCUTS_SEEDS_H
+
+typedef enum
+{
+    SOURCE,
+    SINK,
+    AMBIGUOUS
+} neighbor_type_t;
+
+typedef struct neighbor
+{
+    uint32_t segment_id;
+    neighbor_type_t neighbor_type;
+} neighbor_t;
+
+typedef struct seed_region
+{
+    uint32_t segment_id;
+    neighbor_t *neighbors;
+} seed_region_t;
+
+#endif //MINCUTS_SEEDS_H
+
+
