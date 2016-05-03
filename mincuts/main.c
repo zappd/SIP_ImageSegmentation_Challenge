ebcb43ec2d92caeef1d2c7d7d90cf47215af5c26
gdportwood <gdportwood@users.noreply.github.com>
2016-05-02 20:21:46 -0400

    Calling readID overlay function before calling segment

--- mincuts/main.c
+++ mincuts/main.c
@@ -2,12 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "seeds.h"
 #include "config.h"
 #include "segmenter.h"
 // #include "graphCuts.h"
 
-static const char *data_file_path   = "/home/zappd/SIP_Challenge_Data/StanfordMemorial_rat1_rot90_crop_bandCrop.img";
-static const char *header_file_path = "/home/zappd/SIP_Challenge_Data/StanfordMemorial_rat1_rot90_crop_bandCrop.img.hdr";
+//static const char *data_file_path   = "/home/zappd/SIP_Challenge_Data/StanfordMemorial_rat1_rot90_crop_bandCrop.img";
+//static const char *header_file_path = "/home/zappd/SIP_Challenge_Data/StanfordMemorial_rat1_rot90_crop_bandCrop.img.hdr";
+static const char *data_file_path   = "StanfordMemorial_rat1_rot90_crop_bandCrop.img";
+static const char *header_file_path = "StanfordMemorial_rat1_rot90_crop_bandCrop.img.hdr";
 
 static const char *png_extension = ".png";
 
@@ -30,16 +33,31 @@ int main(int argc, char *argv[])
 {
     image_info_t image_info;
     char         *segment_image_path;
+    uint32_t ** segment_id_map;
+    seed_region_t * seed_region_arr;
 
     float ***image_cube = readImageCube(data_file_path, header_file_path, &image_info);
 
     printf("Read Finished\n\n");
 
     segment_image_path = (char *) malloc((strlen(data_file_path) + strlen(png_extension) + 1) * sizeof(char));
+    segment_id_map=malloc(image_info.lines*sizeof(uint32_t *));
+    seed_region_arr=malloc(gconf.kcent*sizeof(seed_region_t));
+
+    for (uint32_t line = 0; line < image_info.lines; line++)
+    {
+        segment_id_map[line] = (uint32_t *) malloc(image_info.samples * sizeof(uint32_t));
+    }
+
     strcpy(segment_image_path, data_file_path);
     strcat(segment_image_path, png_extension);
 
-    recursivelySegment(image_cube, &image_info, segment_image_path);
+    getSeedIDFromFile("binid.data",segment_id_map);
+
+
+    //cutSeedRegions(image_cube, image_info, segment_id_map, seed_region_arr, gconf.kcent);
+
+    //recursivelySegment(image_cube, &image_info, segment_image_path);
 
     freeImageCube(image_cube, &image_info);
 
